3cf9e7b9f11c706ae675559eda67bb36836a00d9
Abhinav-bM <abhinavbmanoj@gmail.com>
2025-11-02 14:11:32 +0530

    refactor : .env

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ const app = express();
 
 // connect to MongoDB
 connectDB();
-const FRONTEND_URL = process.env.FRONTEND_URL;
+const FRONTEND_URL_USER = process.env.FRONTEND_URL;
+const FRONTEND_URL_ADMIN = process.env.FRONTEND_URL_ADMIN;
+
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [FRONTEND_URL_USER, FRONTEND_URL_ADMIN],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
